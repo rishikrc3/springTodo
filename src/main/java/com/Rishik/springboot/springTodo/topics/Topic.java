e1e2888e7d3c28a969ef71efbe909fce7927a007
Rishik Chakravarty <rishikrc3@gmail.com>
2024-02-27 20:44:10 +0530

    returning_json

---
+++ src/main/java/com/Rishik/springboot/springTodo/topics/Topic.java
@@ -0,0 +1,22 @@
+package com.Rishik.springboot.springTodo.topics;
+
+public class Topic {
+
+    private String name ;
+
+    public Topic(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void  setName(String name)
+    {
+        this.name = name;
+    }
+
+}
--- src/main/java/com/Rishik/springboot/springTodo/topics/TopicController.java
+++ src/main/java/com/Rishik/springboot/springTodo/topics/TopicController.java
@@ -3,12 +3,20 @@ package com.Rishik.springboot.springTodo.topics;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String topics()
+    public List<Topic> topics()
     {
-        return "ALl topics";
+        Topic [] topicsArray = {
+                new Topic("Java"),
+                new Topic("Spring"),
+                new Topic("REST")
+        };
+        return Arrays.asList(topicsArray);
     }
 }
